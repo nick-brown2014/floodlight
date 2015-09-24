1a6da1e08eff405b189d5a5507812c9c5d4d3867
Nick Brown <nick.brown2014@gmail.com>
2015-09-24 13:50:56 -0500

    built scaffold for backend, untested

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+end
---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,21 @@
+class VideosController < ApplicationController
+
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+end
