0804126758f9a46d779c1581ff000c95e9a011f3
Nick Brown <nick.brown2014@gmail.com>
2016-01-18 14:07:25 -0700

    added semi-secure login that will not keep out any hackers and may have huge problems later

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,25 @@ class UsersController < ApplicationController
   end
 
   def index
+    if User.find(1)[:name] != "true"
+      redirect_to root_path
+    end
   end
 
   def new
+    User.find(1).update(name: "admin")
+  end
+
+  def create
+    @password = params[:user][:password]
+    @username = params[:user][:username]
+
+    if @password == "strokin" && @username == "elfoolio"
+      User.find(1).update(name: "true")
+      redirect_to users_path
+    else
+      redirect_to root_path
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  @isAdmin = false
 end
